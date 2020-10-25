3ff123bb09c4a93edc2e9ca2bfd1f8012810583f
Peter Walsh <63989572+Peteylumpkins@users.noreply.github.com>
2020-10-24 23:16:27 -0400

    Update Interface.java

--- BankingSystem/Interface.java
+++ BankingSystem/Interface.java
@@ -1,49 +1,66 @@
 package BankingSystem;
 
-import java.util.Scanner;
-
 public class Interface {
 
     private Bank bank;
-
-    public int runBank() {
-
-        // todo
-
-        boolean run = true;
-        while(run) {
-            printInitialOptions();
-            int choice = choice();
-
-            switch (choice) {
-
-                case 1:
-                    createAccount();
-                    break;
-
-                case 2:
-
-            }
-        }
-        return -1;
-    }
-
-    public int runLogin(String cardNumber) {
-        // todo
-        return -1;
-    }
 
     public Interface(Bank bank) {
         this.bank = bank;
     }
 
+    public void run() {
+        int choice = 1;
+
+        while (choice != 0) {
+            printInitialOptions();
+            choice = choice();
+
+            switch (choice) {
+
+                case 1:
+                    printCreatedCard();
+                    break;
+                case 2:
+                    Card card = login();
+                    if (card == null) {
+                        System.out.println();
+                        System.out.println("Wrong card number or PIN!");
+                    } else {
+                        System.out.println();
+                        System.out.println("You have successfully logged in!");
+                        choice = runLogin(card);
+                        System.out.println();
+                        System.out.println("You have successfully logged out!");
+                    }
+                    break;
+            }
+            System.out.println();
+            System.out.println("Bye!");
+        }
+    }
+
+    private int runLogin(Card card) {
+        printLoginOptions();
+        int choice = choice();
+        while (choice != 0 && choice != 2) {
+            System.out.println();
+            System.out.println("Balance:" + card.getBalance());
+            printLoginOptions();
+            choice = choice();
+        }
+        return choice;
+    }
+
     private void printInitialOptions() {
+        System.out.println();
         System.out.println("1. Create an account");
         System.out.println("2. Log into account");
         System.out.println("0. Exit");
     }
 
-    private void printCreatedCard(Bank.Card card) {
+    private void printCreatedCard() {
+        Card card = this.bank.createCard();
+        System.out.println();
         System.out.println("Your card has been created");
         System.out.println("Your card number: ");
         System.out.println(card.getNUMBER());
@@ -52,18 +69,20 @@ public class Interface {
     }
 
     private void printLoginOptions() {
+        System.out.println();
         System.out.println("1. Balance");
         System.out.println("2. Log out");
         System.out.println("0. Exit");
     }
 
-    private boolean login() {
+    private Card login() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your card number");
-        String cardNumber = scan.next();
-        System.out.print("Enter your pin");
-        String pin = scan.next();
-        return bank.hasCardAccess(cardNumber, pin);
+        System.out.println();
+        System.out.println("Enter your card number:");
+        String cardNumber = scan.nextLine().trim();
+        System.out.println("Enter your pin:");
+        String pin = scan.nextLine().trim();
+        return bank.getCard(cardNumber, pin);
     }
 
     private int choice() {
@@ -71,8 +90,4 @@ public class Interface {
         return scan.nextInt();
     }
 
-    private void createAccount() {
-        Bank.Card card = bank.createCard();
-        printCreatedCard(card);
-    }
 }
