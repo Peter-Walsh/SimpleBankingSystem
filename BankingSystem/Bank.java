15342bba8a8f7fdbe3e91f28aa57afbcb33eca9e
Peter Walsh <63989572+Peteylumpkins@users.noreply.github.com>
2020-12-27 11:28:41 -0500

    Update Bank.java

--- BankingSystem/Bank.java
+++ BankingSystem/Bank.java
@@ -1,9 +1,8 @@
-package BankingSystem;
-import java.util.*;
+package banking;
 
-import javafx.scene.control.Tab;
-import org.sqlite.SQLiteDataSource;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 public class Bank {
 
@@ -16,7 +15,7 @@ public class Bank {
 
     public Card createCard() {
         Card card = new Card();
-        this.dataBase.add(card.id, card.NUMBER, card.PIN, card.balance);
+        this.dataBase.add(card.getId(), card.getNUMBER(), card.getPIN(), card.getBalance());
         numAccounts++;
         return card;
     }
@@ -34,6 +33,50 @@ public class Bank {
         }
     }
 
+    public void deleteAccount(String cardNumber) {
+        dataBase.deleteRow(cardNumber);
+    }
+
+    public void deposit(String cardNumber, int amount) throws InvalidCardException {
+        if (!isValidCard(cardNumber)) {
+            throw new InvalidCardException();
+        }
+        Card card = getCard(cardNumber);
+        if (card == null) {
+            throw new NoSuchElementException();
+        }
+        dataBase.updateBalance(amount, cardNumber);
+    }
+
+    public Card getCard(String cardNumber) {
+        ResultSet row = dataBase.getRow(cardNumber);
+        try {
+            String id = row.getString("id");
+            String cardNum = row.getString("number");
+            String pinNum = row.getString("pin");
+            String balance = row.getString("balance");
+            return new Card(Integer.parseInt(id), cardNum, pinNum, Integer.parseInt(balance));
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    public boolean isValidCard(String cardNumber) {
+        int sum = 0;
+        int next;
+        for (int i = 0; i < cardNumber.length() - 1; i++) {
+            next = Integer.parseInt(String.valueOf(cardNumber.charAt(i)));
+            if (i % 2 == 0) {
+                next *= 2;
+                if (next > 9) {
+                    next -= 9;
+                }
+            }
+            sum += next;
+        }
+        sum += Integer.parseInt(String.valueOf(cardNumber.charAt(cardNumber.length() - 1)));
+        return sum % 10 == 0;
+    }
 }
 
 
